f27e2e8e6ce98e227a4ecde6ca9c6a1029409fd7
wilsonmichaelunl <wilson.michaelthomas@gmail.com>
2017-12-20 15:37:16 -0600

    Fixed crashing and rotate reset

--- app/src/main/java/carlcrate/come/coffeeapp/MainActivity.java
+++ app/src/main/java/carlcrate/come/coffeeapp/MainActivity.java
@@ -6,19 +6,34 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
 
+import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
 
 public class MainActivity extends Activity {
     private TimeCalc newTime = new TimeCalc();
+    private TextView display;
+    private static final String KEY_TEXT_VALUE = "textValue";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display = (TextView) findViewById(R.id.instruction);
+        if(savedInstanceState != null){
+            CharSequence savedText = savedInstanceState.getCharSequence(KEY_TEXT_VALUE);
+            display.setText(savedText);
+        }
     }
 
-    public void onClickFindTime(View view) throws ParseException {
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(KEY_TEXT_VALUE, display.getText());
+    }
+
+    public void onClickFindTime(View view) throws ParseException, InvocationTargetException {
         //Get a reference to the TextView
-        TextView brands = (TextView) findViewById(R.id.instruction);
+        display = (TextView) findViewById(R.id.instruction);
         //Get a reference to the first EditText
         EditText desiredTime = (EditText) findViewById(R.id.desired_time);
         //Get a reference to the second EditText
@@ -30,6 +45,6 @@ public class MainActivity extends Activity {
         //Get time from the TimeCalc Class
         String timeToShow = newTime.getTime(brewTime, machineTime);
         //Display the selected item
-        brands.setText("Set your coffee maker to make coffee at " + timeToShow);
+        display.setText(timeToShow);
     }
 }
--- app/src/main/java/carlcrate/come/coffeeapp/TimeCalc.java
+++ app/src/main/java/carlcrate/come/coffeeapp/TimeCalc.java
@@ -4,6 +4,7 @@ package carlcrate.come.coffeeapp;
  * Created by michael on 12/18/17.
  */
 
+import java.lang.reflect.InvocationTargetException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,39 +13,43 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class TimeCalc {
-    String getTime(String endTime, String timeOnMaker) throws ParseException {
+    String getTime(String endTime, String timeOnMaker) throws ParseException, InvocationTargetException {
         //These next three lines take in the current time
-        DateFormat df = new SimpleDateFormat("HH:mm");
-        Date dateobj = new Date();
-        String timeNow = (df.format(dateobj));
+       try {
+           DateFormat df = new SimpleDateFormat("HH:mm");
+           Date dateobj = new Date();
+           String timeNow = (df.format(dateobj));
 
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-        Date date1 = format.parse(timeNow);
-        Date date2 = format.parse(endTime);
+           SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+           Date date1 = format.parse(timeNow);
+           Date date2 = format.parse(endTime);
 
-        if(date1.getTime() > date2.getTime()) {
-            Calendar c = Calendar.getInstance();
-            c.setTime(date2);
-            c.add(Calendar.DATE, 1);
-            date2 = c.getTime();
-        }
+           if (date1.getTime() > date2.getTime()) {
+               Calendar c = Calendar.getInstance();
+               c.setTime(date2);
+               c.add(Calendar.DATE, 1);
+               date2 = c.getTime();
+           }
 
-        long differenceInSeconds = (date2.getTime() - date1.getTime()) / 1000;
-        long differenceInMinutes = differenceInSeconds / 60;
-        long differenceInHours = differenceInMinutes / 60;
-        long leftoverMinutes = differenceInMinutes % 60;
+           long differenceInSeconds = (date2.getTime() - date1.getTime()) / 1000;
+           long differenceInMinutes = differenceInSeconds / 60;
+           long differenceInHours = differenceInMinutes / 60;
+           long leftoverMinutes = differenceInMinutes % 60;
 
-        int minutesAhead = (int)(differenceInMinutes * 1.2);
-        differenceInHours = minutesAhead / 60;
-        leftoverMinutes = minutesAhead % 60;
+           int minutesAhead = (int) (differenceInMinutes * 1.2);
+           differenceInHours = minutesAhead / 60;
+           leftoverMinutes = minutesAhead % 60;
 
-        Date date3 = format.parse(timeOnMaker);
+           Date date3 = format.parse(timeOnMaker);
 
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date3);
-        cal.add(Calendar.MINUTE, minutesAhead);
-        String newTime = format.format(cal.getTime());
+           Calendar cal = Calendar.getInstance();
+           cal.setTime(date3);
+           cal.add(Calendar.MINUTE, minutesAhead);
+           String newTime = format.format(cal.getTime());
 
-        return newTime;
+           return "Set your coffee maker to make coffee at " + newTime;
+       } catch (ParseException e){
+           return "Your input was invalid. Please try again.";
+       }
     }
 }
